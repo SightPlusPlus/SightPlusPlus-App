432b139d273c6fac657afe42215d3deadf6e7b84
Vlad Udrescu <vladudri@gmail.com>
2021-03-16 09:36:54 +0200

    first commit

---
+++ lib/listen.dart
@@ -0,0 +1,26 @@
+//import 'package:flutter/material.dart';
+//import 'package:sight/ui.dart';
+//import 'package:sight/user_interface.dart';
+//
+//class Listen {
+//  String transcript = 'start';
+//
+//  Listen();
+////  Listen({this.transcript});
+//
+//  Future<void> choose() async {
+//    switch (transcript) {
+//      case 'start':
+//        {
+//          print ('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>' + transcript);
+//          Socket socket = new Socket();
+//          await socket.fc();
+//          break;
+//        }
+//      case 'change voice':
+//        {
+//          break;
+//        }
+//    }
+//  }
+//}
